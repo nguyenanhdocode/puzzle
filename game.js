73710551a3fa123fc7880e439108e8a2f5e46718
Nguyễn Anh Đô <nguyenanhdo200213@gmail.com>
2022-01-15 12:16:45 +0700

    edit files

--- game.js
+++ game.js
@@ -1,50 +1,68 @@
 const BOARD_WIDTH = 560
 const BOARD_HEIGHT = 560
-var rows = 3
-var cols = 3
-const PIECE_WIDTH = BOARD_WIDTH / cols
-const PIECE_HEIGHT = BOARD_HEIGHT / rows
+var rows = 0
+var cols = 0
+var piece_width = 0
+var piece_height = 0
 
-var imageSrc = 'images/1.jpg'
-var image
+var imageSrc = ''
+var image = document.createElement('img')
 var boardMatrix = []
 var pieces = []
 
-var rootElement
-var board
+var container = document.getElementById('container')
+var play = document.getElementById('play')
+var board = document.getElementById('board')
+var selectedIndex = []
+var moved = 0
 
 
-layoutInitialize()
-
-startGame()
-
-
-function layoutInitialize() {
-    rootElement = document.getElementById('root')
-
-    var playPage = document.createElement('div')
-    playPage.id = 'play'
-
-    rootElement.appendChild(playPage)
-    
-    var title = document.createElement('h1')
-    title.id = 'title'
-    title.innerText = 'Puzzle'
-    playPage.appendChild(title)
-
-    board = document.createElement('div')
-    board.id = 'board'
-    playPage.appendChild(board)
-
+// Load image
+var imagesDiv = document.getElementById('images')
+var formTitle = document.getElementById('form-title')
+var form = document.getElementById('form')
+var sizeDiv = document.getElementById('size')
+for (let i = 1; i <= 4; i++) {
+    let img = document.createElement('img')
+    img.src = `images/${i}.jpg`
+    img.classList.add('images-item')
+    imagesDiv.appendChild(img)
+    img.onclick = function(e) {
+        var src = e.target.getAttribute('src')
+        imageSrc = src
+        imagesDiv.style.display = 'none'
+        formTitle.innerText = 'Choose board size to start'
+        sizeDiv.style.display = 'flex'
+    }
 }
 
-function startGame() {
-    image = document.createElement('img')
-    image.src = imageSrc
 
+for (let i = 0; i < sizeDiv.childNodes.length; i++) {
+    sizeDiv.childNodes[i].onclick = function(e) {
+        var r = parseInt(e.target.getAttribute('r'))
+        var c = parseInt(e.target.getAttribute('c'))
+        rows = r
+        cols = c
+        selectedIndex = [rows - 1, cols - 2]
+        piece_width = BOARD_WIDTH / cols
+        piece_height = BOARD_HEIGHT / rows
+
+        document.getElementById('img-preview').setAttribute('src', imageSrc)
+
+        play.style.display = 'block'
+        form.style.display = 'none'
+
+        startGame()
+    }
+}
+
+
+function startGame() {
+    image.src = imageSrc
     
     image.onload = function() {
         pieces = shuffle(createPieces())
+
         var i = 0
         for (let r = 0; r < rows; r++) {
             let row = []
@@ -59,11 +77,59 @@ function startGame() {
         whitePiece.id = 'white-piece'
         whitePiece.classList.add('piece')
         whitePiece.style.display = 'inline-block'
-        whitePiece.style.width = `${PIECE_WIDTH}px`
-        whitePiece.style.height = `${PIECE_HEIGHT}px`
+        whitePiece.style.width = `${piece_width - 3}px`
+        whitePiece.style.height = `${piece_height - 3}px`
         boardMatrix[rows - 1][cols - 1] = whitePiece
+        boardMatrix[selectedIndex[0]][selectedIndex[1]].classList
+        .add('selected')
 
         draw()
+    }
+
+    document.onkeydown = function(e) {
+        /**
+         * UP:      38
+         * DOWN:    40
+         * LEFT:    37
+         * RIGHT:   39
+         */
+        var prevIndex = [selectedIndex[0], selectedIndex[1]]
+        switch(e.which) {
+            case 38: {
+                moveFrame('UP')
+                if (e.ctrlKey) {
+                    swapPiece(prevIndex, selectedIndex)
+                    draw()
+                }
+                break
+            }
+            case 40: {
+                moveFrame('DOWN')
+                if (e.ctrlKey) {
+                    swapPiece(prevIndex, selectedIndex)
+                    draw()
+                }
+                break
+            }
+            case 37: {
+                moveFrame('LEFT')
+                if (e.ctrlKey) {
+                    swapPiece(prevIndex, selectedIndex)
+                    draw()
+                }
+                break
+            }
+            case 39: {
+                moveFrame('RIGHT')
+                if (e.ctrlKey) {
+                    swapPiece(prevIndex, selectedIndex)
+                    draw()
+                }
+                break
+            }
+            default:
+                break
+        }
     }
 
 }
@@ -78,15 +144,19 @@ function createPieces() {
 
             if (count <= (rows * cols - 1)){
                 var canvas = document.createElement('canvas')
-                canvas.width = PIECE_WIDTH
-                canvas.height = PIECE_HEIGHT
+                canvas.width = piece_width
+                canvas.height = piece_height
                 var context = canvas.getContext('2d')
-                context.drawImage(image, c * PIECE_WIDTH, r * PIECE_HEIGHT, PIECE_WIDTH, PIECE_HEIGHT, 0, 0, canvas.width, canvas.height)
-                
-                var img = document.createElement('img')
+                context.drawImage(image, c * piece_width, r * piece_height, piece_width, piece_height, 0, 0, canvas.width, canvas.height)
+                console.log(canvas)
+                let img = document.createElement('img')
                 img.src = canvas.toDataURL()
+                img.style.width = `${piece_width -3}px`
+                img.style.height = `${piece_height - 3}px`
+                img.id = `${r}${c}`
                 img.classList.add(...['piece',])
                 pieces.push(img)
+                
             }
 
             count += 1
@@ -122,4 +192,76 @@ function draw() {
             board.appendChild(boardMatrix[r][c])
         }
     }
+}
+
+
+
+function moveFrame(dir) {
+    var prevPiece = boardMatrix[selectedIndex[0]][selectedIndex[1]]
+    switch(dir) {
+        case 'RIGHT': {
+            if (selectedIndex[1] + 1 < cols) {
+                selectedIndex[1] += 1
+            }
+            else {
+                selectedIndex[1] = 0
+            }
+            break
+        }
+        case 'LEFT': {
+            if (selectedIndex[1] - 1 >= 0) {
+                selectedIndex[1] -= 1
+            }
+            else {
+                selectedIndex[1] = cols - 1
+            }
+            break
+        }
+        case 'UP': {
+            if (selectedIndex[0] - 1 >= 0) {
+                selectedIndex[0] -= 1
+            }
+            else {
+                selectedIndex[0] = rows - 1
+            }
+            break
+        }
+        case 'DOWN': {
+            if (selectedIndex[0] + 1 < rows) {
+                selectedIndex[0] += 1
+            }
+            else {
+                selectedIndex[0] = 0
+            }
+            break
+        }
+        default:
+            break
+    }
+
+    var nextPiece = boardMatrix[selectedIndex[0]][selectedIndex[1]]
+
+    prevPiece.classList.remove('selected')
+    nextPiece.classList.add('selected')
+}
+
+
+function swapPiece(from, to) {
+    var movedPiece = boardMatrix[from[0]][from[1]]
+    var whitePiece = boardMatrix[to[0]][to[1]]
+
+    if (boardMatrix[to[0]][to[1]].id == 'white-piece') {
+
+        var tmp = boardMatrix[from[0]][from[1]]
+        boardMatrix[from[0]][from[1]] = boardMatrix[to[0]][to[1]]
+        boardMatrix[to[0]][to[1]] = tmp
+    
+        movedPiece.classList.add('selected')
+        whitePiece.classList.remove('selected')
+
+        moved += 1
+
+        document.getElementById('moved').innerText = `Move: ${moved}`
+    }
+
 }
